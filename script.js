265faea910b7d7deb691c493e6e4dc4436a9d45e
Tanuka Sengupta <tanuka0202@gmail.com>
2025-12-08 12:09:33 +0530

    Refactor phone validation and JSONP handling

--- script.js
+++ script.js
@@ -96,13 +96,13 @@ form.addEventListener("submit", async (e) => {
       // FINAL REDIRECT LOGIC
       if (
         mainStatus === "all done" &&
-        verificationStatus === "Done" &&
+        verificationStatus === "yes" &&
         groundVerificationStatus === "done"
       ) {
         window.location.href = `vehicle.html?phone=${phone}`;
       } else if (
         mainStatus === "all done" &&
-        verificationStatus === "Done"
+        verificationStatus === "yes"
       ) {
         window.location.href = `onground.html?phone=${phone}`;
       } else if (mainStatus === "all done") {
@@ -135,6 +135,7 @@ form.addEventListener("submit", async (e) => {
     status.style.color = "red";
   }
 });
+
 
 
 
