42ea9aaa84a962182d1fc75572d05f317c4d7aa0
Tanuka Sengupta <tanuka0202@gmail.com>
2025-12-09 10:46:30 +0530

    Change POST request content type to 'text/plain'
    Updated the content type for the POST request to 'text/plain'.

--- followup3.js
+++ followup3.js
@@ -75,7 +75,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fetch(WEB_APP_URL, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: { "Content-Type": "text/plain" },
       body: JSON.stringify(data)
     })
       .then(res => res.json())
@@ -105,3 +105,4 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 });
+
