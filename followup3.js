49ad82ebcf46d57ba836d68fea0301fe5a7a01c1
Arka Mondal <arkamondal448@gmail.com>
2025-12-12 23:36:58 +0530

    Update followup3.js

--- followup3.js
+++ followup3.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("followupForm");
   const status = document.getElementById("status");
 
-  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzi3ePMz096OdHxQK0POMDTgZaloiB3fz8LprpWftJHViRn_NDeXFHWst2OjJ2-37JtDQ/exec";
+  const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwCc1J-ypHeUKSt7jXMu24Vq1sy7NuI-DXoPj8l_1TsjGYlAlYhEKWijIeUxHs1asuQfg/exec";
   const urlParams = new URLSearchParams(window.location.search);
   const phoneFromLogin = urlParams.get("phone");
 
@@ -99,6 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
       });
   });
 });
+
 
 
 
